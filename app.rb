18d5ad92073c9baf0a8906b8429a98f9a36c5658
Joey Aghion <joey@aghion.com>
2014-05-09 17:58:06 -0400

    Add API auth

--- app.rb
+++ app.rb
@@ -5,9 +5,23 @@ PORT = ENV['PORT'] || 4567
 
 set :port, PORT
 
+authorized_keys = []
+
+use Rack::Auth::Basic, "Restricted Area" do |api_key, _|
+  $stderr.puts "BASIC : #{api_key}"
+  authorized_keys.include?(api_key)
+end
+
+before { content_type :json }
+
 get '/api/widgets' do
-  content_type :json
   Widgets.all.to_json
+end
+
+post '/api/clients' do
+  client = { key: SecureRandom.hex(32) }
+  authorized_keys << client[:key]
+  client.to_json
 end
 
 class Widgets
